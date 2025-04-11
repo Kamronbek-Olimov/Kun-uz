580808e298d9426609c7c085985535f3be25ec44
Kamronbek Olimov <qobiljonovichkamronbek@gmail.com>
2025-04-11 10:42:45 +0500

    upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 document.querySelector(".menu-btn").addEventListener("click", ()=>{
     document.querySelector(".menu-links").classList.toggle("active")    
 })
+document.querySelector(".search-btn").addEventListener("click", ()=>{
+    document.querySelector(".search-input").classList.add("active")
+})
+document.querySelector(".remove-search").addEventListener("click", ()=>{
+    document.querySelector(".search-input").classList.remove("active")
+})
