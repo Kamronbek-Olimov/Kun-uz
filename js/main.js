1fc5efe89c77935f68a65341e3d0b1554d87ef44
Kamronbek Olimov <qobiljonovichkamronbek@gmail.com>
2025-04-11 20:34:16 +0500

    fdgffge

--- js/main.js
+++ js/main.js
@@ -7,3 +7,23 @@ document.querySelector(".search-btn").addEventListener("click", ()=>{
 document.querySelector(".remove-search").addEventListener("click", ()=>{
     document.querySelector(".search-input").classList.remove("active")
 })
+document.addEventListener("DOMContentLoaded", function () {
+    const selectBtn = document.querySelector(".select-bar .select-btn");
+    const langs = document.querySelector(".select-bar .langs");
+    const currentLang = selectBtn.querySelector("span")
+    selectBtn.addEventListener("click", function (e) {
+        e.stopPropagation()
+        langs.classList.toggle("active");
+    })
+    langs.querySelectorAll("span").forEach(function (langOption) {
+        langOption.addEventListener("click", function (e) {
+            currentLang.textContent = this.textContent;
+            langs.classList.remove("active");
+        });
+    })
+    document.addEventListener("click", function (e) {
+        if (!document.querySelector(".select-bar").contains(e.target)) {
+            langs.classList.remove("active");
+        }
+    });
+})
